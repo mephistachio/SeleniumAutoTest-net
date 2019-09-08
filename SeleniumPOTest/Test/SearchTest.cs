77f203753959c174492d509ee6cea20b9b5189d7
mephistachio <47401980+mephistachio@users.noreply.github.com>
2019-09-08 16:35:50 +0200

    v0.02

--- SeleniumPOTest/Test/SearchTest.cs
+++ SeleniumPOTest/Test/SearchTest.cs
@@ -2,7 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using SeleniumPOTest.Pages;
-using System;
+
 namespace SeleniumPOTest.Test
 {
     class SearchTest
@@ -10,14 +10,11 @@ namespace SeleniumPOTest.Test
         IWebDriver webDriver;
 
         [SetUp]
-
         public void IntBrowser()
         {
             webDriver = new FirefoxDriver();
             webDriver.Navigate().GoToUrl("https://www.dnvgl.com/");
             webDriver.Manage().Window.Maximize();
-           
-
         }
 
         [Test]
@@ -25,10 +22,10 @@ namespace SeleniumPOTest.Test
         public void SearchTestFF()
         {
             HomePage objSearch = new HomePage(webDriver);
-           objSearch.SearchBtn.Click();
+            objSearch.SearchBtn.Click();
             var searchResult = objSearch.SearchgBox("2019");
             Assert.AreEqual(5, searchResult.Count);
-           
+
         }
         [Test]
         //negative test
@@ -38,7 +35,6 @@ namespace SeleniumPOTest.Test
             objSearch.SearchBtn.Click();
             var searchResult = objSearch.SearchgBox("#@)!$0");
             Assert.AreEqual(0, searchResult.Count);
-
         }
     }
 }
